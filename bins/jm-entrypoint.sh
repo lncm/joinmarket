4ff11eb4c790a7bc895ed4a5130c0e2ef98466fe
nolim1t <hello@nolim1t.co>
2020-09-30 16:13:11 +0700

    Add some stuff to jm-entrypoint

--- bins/jm-entrypoint.sh
+++ bins/jm-entrypoint.sh
@@ -1,6 +1,37 @@
 #!/bin/bash
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
 
 # inital variables
 JMHOMEDIR=${HOME}/.joinmarket
+JMWALLETDIR=${JMHOMEDIR}/wallets
+JMWALLET=wallet.jmdat
+# Define secrets
+JMSEEDFILE=${JMHOMEDIR}/jm-wallet-seed
+JMWALLETPASS=${JMHOMEDIR}/jm-wallet-password
+
+# dep check
+check_dependencies () {
+    for cmd in "$@"; do
+        if ! command -v $cmd >/dev/null 2>&1; then
+            echo "This script requires \"${cmd}\" to be installed"
+            exit 1
+        fi
+    done
+}
+check_dependencies pwgen
 
 # setup script
+if [ ! -f $JMWALLETDIR/$JMWALLET ]; then
+    if [ !  -f $JMWALLETPASS ]; then
+        echo "Making password"
+        echo `pwgen -s 21 -1 -v -c -0` > $JMWALLETPASS
+    fi
+    echo "Creating wallet"
+fi
+
